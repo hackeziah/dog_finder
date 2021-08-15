d5ff6c12e953242777eae8808325e728d586d632
hackeziah <lamadridkevinpaul@gmail.com>
2021-08-15 22:03:09 +0800

    adding login tempalte and registration dogs

--- dog_finder/settings.py
+++ dog_finder/settings.py
@@ -126,7 +126,11 @@ TIME_ZONE = 'Asia/Manila'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
